17e2cc713974e7e0579f96d9b9c2c56dda75dec5
MrTitusZ <novak.csaba.titusz@gmail.com>
2024-02-04 20:40:36 +0100

    Added initial implementation of the tic-tac-toe field drawing.

--- Tic-Tac-Toe/src/Main.cpp
+++ Tic-Tac-Toe/src/Main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include "DrawField.h"
 
-const int sizeOfFiled = 5;
+const int sizeOfFiled = 3;
 const char fullBlock = char(219);
 
-int main () {
-	int frame = 0;
-	std::cout << "-------------" << std::endl;
-	for (int i = 0; i < 3; i++) {
-		std::cout << "|";
-		for (int j = 0; j < 3; j++) {
-			std::cout << " " << "*" << " " << "|";
+int main() {
+	int field[sizeOfFiled][sizeOfFiled]{};
+	for (int i = 0; i < sizeOfFiled; i++) {
+		for (int j = 0; j < sizeOfFiled; j++) {
+			field[i][j] = 1;
 		}
-		std::cout<< std::endl << "-------------" << std::endl;
+	}
+
+	std::cout << "------------" << std::endl;
+	for (int i = 0; i < sizeOfFiled; i++) {
+		for (int j = 0; j < sizeOfFiled; j++) {
+			std::cout << " " << field[i][j] << " " << "|";
+		}
+		std::cout << std::endl << "------------" << std::endl;
 	}
 	return 0;
 }
+
