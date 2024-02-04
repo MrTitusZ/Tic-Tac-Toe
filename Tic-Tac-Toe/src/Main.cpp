dbb49869f5333541981c278c0ce3661283499010
MrTitusZ <novak.csaba.titusz@gmail.com>
2024-02-04 18:09:37 +0100

    3x3 tic-tac

--- Tic-Tac-Toe/src/Main.cpp
+++ Tic-Tac-Toe/src/Main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
 
 const int sizeOfFiled = 5;
 const char fullBlock = char(219);
 
-int main() {
+int main () {
 	int frame = 0;
-	while(true) {
-
+	std::cout << "-------------" << std::endl;
+	for (int i = 0; i < 3; i++) {
+		std::cout << "|";
+		for (int j = 0; j < 3; j++) {
+			std::cout << " " << "*" << " " << "|";
+		}
+		std::cout<< std::endl << "-------------" << std::endl;
 	}
-	std::cout << "Waiting..." << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(3));
 	return 0;
 }
